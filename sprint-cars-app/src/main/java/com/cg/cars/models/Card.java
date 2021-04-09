5a30076ff005b91a4233b24529d4cb0f9b611686
Nikhil <Lenovo@DESKTOP-LAPPAFS>
2021-04-09 20:13:19 +0530

    Commit

--- sprint-cars-app/src/main/java/com/cg/cars/models/Card.java
+++ sprint-cars-app/src/main/java/com/cg/cars/models/Card.java
@@ -3,8 +3,59 @@ package com.cg.cars.models;
 import java.time.LocalDate;
 
 public class Card {
-	private String cardName;
-	private String cardNumber;
-    private LocalDate cardExpiry;
+	private String name;
+	private String number;
+    private LocalDate expiry;
     private int cvv;
+    
+	public Card() {
+	}
+
+	public Card(String name, String number, LocalDate expiry, int cvv) {
+		this.name = name;
+		this.number = number;
+		this.expiry = expiry;
+		this.cvv = cvv;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public LocalDate getExpiry() {
+		return expiry;
+	}
+
+	public void setExpiry(LocalDate expiry) {
+		this.expiry = expiry;
+	}
+
+	public int getCvv() {
+		return cvv;
+	}
+
+	public void setCvv(int cvv) {
+		this.cvv = cvv;
+	}
+
+	@Override
+	public String toString() {
+		return "Card [name=" + name + ", number=" + number + ", expiry=" + expiry + ", cvv=" + cvv + "]";
+	}
+    
+    
+    
+    
 }
--- sprint-cars-app/src/main/java/com/cg/cars/models/Payment.java
+++ sprint-cars-app/src/main/java/com/cg/cars/models/Payment.java
@@ -1,8 +1,46 @@
 package com.cg.cars.models;
 
 public class Payment {
-	private long paymentId;
+	private long id;
 	private String type;
 	private String status;
 	private Card card;
+	public Payment() {
+	}
+	public Payment(long id, String type, String status, Card card) {
+		this.id = id;
+		this.type = type;
+		this.status = status;
+		this.card = card;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Card getCard() {
+		return card;
+	}
+	public void setCard(Card card) {
+		this.card = card;
+	}
+	@Override
+	public String toString() {
+		return "Payment [id=" + id + ", type=" + type + ", status=" + status + ", card=" + card + "]";
+	}
+	
+	
 }
